07e3b4850b6c5c8715f7765c1422ab032ca36de7
Bruno <bruno.conrad12@gmail.com>
2022-11-20 17:39:51 -0300

    Adicionado navbar closes #5

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NavBar from './components/Menu';
 import Inicio from './Inicio';
 import CtrlUsuario from './usuarios/CtrlUsuario';
 
 export default function AppRouter() {
     return (
         <Router>
+            <NavBar />
             <Routes>
                 <Route path='/' element={<Inicio />} />
                 <Route path='/usuarios' element={<CtrlUsuario />} />
